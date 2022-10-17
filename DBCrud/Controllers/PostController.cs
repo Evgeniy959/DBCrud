7055d2e6a988252e09b4e0179024ab766b88d5bd
Pili_zr36 <Pili_zr36@itstepru.onmicrosoft.com>
2022-10-17 22:12:38 +0700

    update

--- DBCrud/Controllers/PostController.cs
+++ DBCrud/Controllers/PostController.cs
@@ -1,7 +1,9 @@
 ﻿using DBCrud.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
