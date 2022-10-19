c30fadf638189fb44b93b77eea63664a235a9e59
Pili_zr36 <Pili_zr36@itstepru.onmicrosoft.com>
2022-10-19 12:08:49 +0700

    update DB

--- DBCrud/Migrations/20221019050433_columChange.Designer.cs
+++ DBCrud/Migrations/20221019050433_columChange.Designer.cs
@@ -4,14 +4,16 @@ using DBCrud.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DBCrud.Migrations
 {
     [DbContext(typeof(BlogDbContext))]
-    partial class BlogDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221019050433_columChange")]
+    partial class columChange
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
